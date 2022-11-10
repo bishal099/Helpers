6ee4b11901e7ad90e9f94d4323a7f52ecf16ff61
Bishal Dhakal <51736169+bishal099@users.noreply.github.com>
2022-11-10 20:43:17 +0545

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,1 +1,16 @@
+//preventSpecialCharactersJS
+nameKeydown(e) {
+  if (/^[!@#\$%\^&*\)\(+=._-]+$/g.test(e.key)) {
+    e.preventDefault();
+  }
+},
+  
+  //dynamicObjAssignVue
+  Object.assign(dynamicObject, {
+        [imageFileName]: event.target.files[0],
+      })
 
+      Object.assign(dynamicObject, {
+        [imagePreviewName]: URL.createObjectURL(dynamicObject[imageFileName])
+      })
+      console.log(dynamicObject[imageFileName])
